5d47e7482c0a7d1d55e9fee24cf97e6c98e9e939
Jonáš Rosecký <rosecky.jonas@gmail.com>
2025-12-11 22:51:58 +0100

    temp logging

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,12 @@ const commands: {
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
+console.log(commandFolders);
 
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter((file) => file === 'index.ts');
+    console.log(fs.readdirSync(commandsPath));
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
